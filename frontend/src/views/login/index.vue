19227d655c775fafe09159caf92516f1b08d0e89
longxiaoming <1781092881@qq.com>
2023-03-24 21:54:06 +0800

    Update index.vue

--- frontend/src/views/login/index.vue
+++ frontend/src/views/login/index.vue
@@ -5,8 +5,6 @@
       <div class="title-container">
         <h3 class="title">登录</h3>
       </div>
-      <!-- <el-tabs v-model="activeName" :stretch="true">
-        <el-tab-pane label="管理员登录" name="first" class="item"> -->
       <el-form-item prop="username">
         <span class="svg-container">
           <svg-icon icon-class="user" />
@@ -45,49 +43,6 @@
           </span>
         </el-form-item>
       </el-tooltip>
-      <!-- </el-tab-pane> -->
-
-      <!-- <el-tab-pane label="用户登录" name="second" width="50%">
-          <el-form-item prop="username">
-            <span class="svg-container">
-              <svg-icon icon-class="user" />
-            </span>
-            <el-input
-              ref="username"
-              v-model="loginForm.username"
-              placeholder="Username"
-              name="username"
-              type="text"
-              tabindex="1"
-              autocomplete="on"
-            />
-          </el-form-item>
-
-          <el-tooltip v-model="capsTooltip" content="Caps lock is On" placement="right" manual>
-            <el-form-item prop="password">
-              <span class="svg-container">
-                <svg-icon icon-class="password" />
-              </span>
-              <el-input
-                :key="passwordType"
-                ref="password"
-                v-model="loginForm.password"
-                :type="passwordType"
-                placeholder="Password"
-                name="password"
-                tabindex="2"
-                autocomplete="on"
-                @keyup.native="checkCapslock"
-                @blur="capsTooltip = false"
-                @keyup.enter.native="handleLogin"
-              />
-              <span class="show-pwd" @click="showPwd">
-                <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-              </span>
-            </el-form-item>
-          </el-tooltip>
-        </el-tab-pane> -->
-      <!-- </el-tabs> -->
       <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
 
     </el-form>
@@ -102,24 +57,7 @@ export default {
   name: 'Login',
   components: { },
   data() {
-    // const validateUsername = (rule, value, callback) => {
-    //   if (!validUsername(value)) {
-    //     callback(new Error('Please enter the correct user name'))
-    //   } else {
-    //     callback()
-    //   }
-    // }
-    // const validatePassword = (rule, value, callback) => {
-    //   if (value.length < 6) {
-    //     callback(new Error('The password can not be less than 6 digits'))
-    //   } else {
-    //     callback()
-    //   }
-    // }
     return {
-
-      //   activeName: 'first',
-
       loginForm: {
         username: '',
         password: '',
@@ -148,9 +86,6 @@ export default {
       immediate: true
     }
   },
-  created() {
-    // window.addEventListener('storage', this.afterQRScan)
-  },
   mounted() {
     if (this.loginForm.username === '') {
       this.$refs.username.focus()
@@ -166,9 +101,6 @@ export default {
         this.loginForm.ip = Ip
         localStorage.setItem('Ip', Ip)
       })
-  },
-  destroyed() {
-    // window.removeEventListener('storage', this.afterQRScan)
   },
   methods: {
     checkCapslock(e) {
@@ -196,33 +128,6 @@ export default {
             .catch(() => {
               this.loading = false
             })
-
-        //   this.loading = true
-        //   const url = '/users/user/login'
-        //   const params = new URLSearchParams()
-        //   params.append('userName', this.loginForm.username)
-        //   params.append('password', md5(this.loginForm.password))
-        //   params.append('ip', this.loginForm.ip)
-        //   params.append('platform', '管理')
-        //   this.$http.post(url, params).then((res) => {
-        //     if (res.status === 200) {
-        //       localStorage.setItem('userId', res.data.data.user.id)
-        //       this.$store.commit('SET_TOKEN', 'admin-token')
-        //       Cookies.set('Admin-Token', 'admin-token')
-        //       this.$message({
-        //         type: 'success',
-        //         message: '登录成功!'
-        //       })
-        //       this.$router.push('/')
-        //       this.loading = false
-        //     } else {
-        //       this.$message({
-        //         type: 'error',
-        //         message: '登录失败!'
-        //       })
-        //     }
-        //     this.loading = false
-        //   })
         } else {
           console.log('登录失败')
           return false
@@ -237,24 +142,6 @@ export default {
         return acc
       }, {})
     }
-    // afterQRScan() {
-    //   if (e.key === 'x-admin-oauth-code') {
-    //     const code = getQueryObject(e.newValue)
-    //     const codeMap = {
-    //       wechat: 'code',
-    //       tencent: 'code'
-    //     }
-    //     const type = codeMap[this.auth_type]
-    //     const codeName = code[type]
-    //     if (codeName) {
-    //       this.$store.dispatch('LoginByThirdparty', codeName).then(() => {
-    //         this.$router.push({ path: this.redirect || '/' })
-    //       })
-    //     } else {
-    //       alert('第三方登录失败')
-    //     }
-    //   }
-    // }
   }
 }
 </script>
